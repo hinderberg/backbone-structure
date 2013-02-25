0e22a4e81a10c8b35e83c6be4cde1632ebfa5e10
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-02-25 19:52:40 +0100

    Made some changes

--- backbone-and-structure/examples/js/modules/transaction/transactionsView.js
+++ backbone-and-structure/examples/js/modules/transaction/transactionsView.js
@@ -13,25 +13,23 @@ var TransactionsView = Backbone.View.extend({
         this.transactions = options.transactions;
     },
     render: function() {
-
-		var sortedTransactions = this.transactions.sortBy(function(transaction) {
+		var sortedTransactions = this.transactions.sortBy(
+          function(transaction) {
             var timestamp = Date.parse(transaction.get('createdAt')) * 1000;
             if (transaction.get("type") === transactionType.ONE_TYPE) {
                 timestamp += "B";
-            } else if (transaction.get("type") === transactionType.ANOTHER_TYPE) {
+            } else if (transaction.get("type") === 
+              transactionType.ANOTHER_TYPE) {
                 timestamp += "A";
             } 
-
-            console.log(timestamp);
 
             return timestamp;
         });
 
-        this.transactions.reset(sortedTransactions);
-		var json = this.transactions.toJSON();
-		this.$el.html(transactionsTemplate(json));
+        var json = _.map(sortedTransactions, function(transaction){ return transaction.toJSON(); });
+        this.$el.html(transactionsTemplate(json));
 
-		return this;
+        return this;
     }
 });
 
